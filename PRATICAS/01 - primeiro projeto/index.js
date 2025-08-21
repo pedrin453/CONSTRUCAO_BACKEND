994f64f6db5b0b5a10fc30ea37438af02b562f48
pedrin453 <pedro.francisco@iesb.edu.br>
2025-08-20 21:30:15 -0300

    Primeiro projeto node

---
+++ PRATICAS/01 - primeiro projeto/index.js
@@ -0,0 +1,20 @@
+console.log("Rodando meu primeiro projeto em NODE")
+
+let nome = "Pedro";
+
+let sobreNome= "Silva";
+
+console.log(nome+ " "+ sobreNome)
+
+
+let idade = 20
+
+console.log( "A idade do" + nome + "e" + idade )
+
+let exercicios = 0.5
+let trabalho = 1.5
+let prova = 4.5
+
+let notaA1Calculada = exercicios + trabalho + prova 
+
+console.log("Nota A1: " + notaA1Calculada)
