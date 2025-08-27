5011c4230ba464729c923a12ee684eb1ff83d88e
pedrin453 <pedro.francisco@iesb.edu.br>
2025-08-27 20:32:46 -0300

    03-exercicio-calculadora

---
+++ PRATICAS/03-exercicio-calculadora-node/calculadora.js
@@ -0,0 +1,27 @@
+function somar(a, b) {
+    return a + b;
+}
+function subtrair(a, b) {
+    return a - b;
+}
+function multiplicar(a, b) {
+    return a * b;
+}
+function dividir(a, b) {
+    return a / b;
+}
+function aoQuadrado(a){
+    return a * a;
+}
+function raizQuadrada(a){
+    return a ** 0,5;
+}
+
+module.exports = {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir,
+    aoQuadrado,
+    raizQuadrada
+}
